90f2428e7980cd3cca7c6dda54d929b313bf72bb
田浪 <tianlang@aiyongtech.com>
2023-05-23 19:01:47 +0800

    FEAT: 使用两个循环生成棋盘格子

--- src/App.jsx
+++ src/App.jsx
@@ -37,25 +37,33 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
+  // 使用两个循环生成棋盘格子
+  const board = Array.from({ length: 3 }).map(
+    (
+      _,
+      i // 生成3行
+    ) => (
+      <div className="board-row">
+        {Array.from({ length: 3 }).map(
+          (
+            _,
+            j // 每行3列
+          ) => (
+            <Square
+              value={squares[i * 3 + j]}
+              onSquareClick={() => handleClick(i * 3 + j)}
+            />
+          )
+        )}
+      </div>
+    )
+  );
+
   // 返回棋盘组件
   return (
     <Fragment>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {board}
     </Fragment>
   );
 }
